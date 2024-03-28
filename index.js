7d9e33b68164d92bb0b312213c618999d70537f7
Abdulhakim Kullow <abdulhakim.kullow@student.moringaschool.com>
2024-03-28 16:51:29 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1;;
+const num3 = 20; 
+const num4 = 8;  
+const mod = (num3 % num4); 
+const numbers = [5, 10, 15, 20, 8];
+const max = Math.max(...numbers);
